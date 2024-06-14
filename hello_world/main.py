7596c14efa41c0448350b5e4607490ed0916f648
Raguvaran Madhiyan <ragu.varan8@gmail.com>
2024-06-14 13:49:20 +0800

    fix by flake8

--- hello_world/main.py
+++ hello_world/main.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def fetch_github_api_status():
     response = requests.get('https://api.github.com')
     if response.status_code == 200:
@@ -7,8 +8,10 @@ def fetch_github_api_status():
     else:
         return 'Hello, World! But could not reach GitHub API.'
 
+
 def main():
     print(fetch_github_api_status())
+
 
 if __name__ == "__main__":
     main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,13 @@
 import unittest
 from hello_world.main import fetch_github_api_status
 
+
 class TestHelloWorld(unittest.TestCase):
     
     def test_fetch_github_api_status(self):
         result = fetch_github_api_status()
         self.assertIn('Hello, World!', result)
+
 
 if __name__ == '__main__':
     unittest.main()
